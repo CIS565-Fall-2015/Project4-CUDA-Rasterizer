d4422db1f22dbd9dc5c9548a4e8c31d043c6efa4
unknown <momeg@MOR100B-04.base.seas.upenn.edu>
2015-10-05 14:43:44 -0400

    first few steps

--- src/rasterize.cu
+++ src/rasterize.cu
@@ -22,6 +22,9 @@ struct VertexIn {
     // TODO (optional) add other vertex attributes (e.g. texture coordinates)
 };
 struct VertexOut {
+	glm::vec3 pos;
+    glm::vec3 nor;
+    glm::vec3 col;
     // TODO
 };
 struct Triangle {
@@ -35,6 +38,7 @@ static int width = 0;
 static int height = 0;
 static int *dev_bufIdx = NULL;
 static VertexIn *dev_bufVertex = NULL;
+static VertexOut *dev_bufTransformedVertex = NULL;
 static Triangle *dev_primitives = NULL;
 static Fragment *dev_depthbuffer = NULL;
 static glm::vec3 *dev_framebuffer = NULL;
@@ -112,6 +116,7 @@ void rasterizeSetBuffers(
     }
     cudaFree(dev_bufVertex);
     cudaMalloc(&dev_bufVertex, vertCount * sizeof(VertexIn));
+	cudaMalloc(&dev_bufTransformedVertex, vertCount * sizeof(VertexOut));
     cudaMemcpy(dev_bufVertex, bufVertex, vertCount * sizeof(VertexIn), cudaMemcpyHostToDevice);
 
     cudaFree(dev_primitives);
@@ -119,6 +124,54 @@ void rasterizeSetBuffers(
     cudaMemset(dev_primitives, 0, vertCount / 3 * sizeof(Triangle));
 
     checkCUDAError("rasterizeSetBuffers");
+}
+
+__global__ void vertexShader(VertexIn* inVerts, VertexOut* outVerts, int vertCount) {
+	int thrId = (blockIdx.x * blockDim.x) + threadIdx.x;
+	if (thrId < vertCount) {
+		outVerts[thrId].pos[0] = (inVerts[thrId].pos[0] * 2.0f) - 1.0f;
+		outVerts[thrId].pos[1] = (inVerts[thrId].pos[1] * 2.0f) - 1.0f;
+		outVerts[thrId].pos[2] = (inVerts[thrId].pos[2] * 2.0f) - 1.0f;
+	}
+
+}
+
+__global__ void primitiveAssemble(VertexOut* verts, Triangle* tris, int vertCount) {
+	int thrId = (blockIdx.x * blockDim.x) + threadIdx.x;
+	if (thrId < vertCount) {
+		if (thrId % 3 == 0) {
+			tris[thrId].v[0] = verts[thrId];
+		} 
+		else if (thrId % 3 == 1) {
+			tris[thrId - 1].v[1] = verts[thrId];
+		}
+		else {
+			tris[thrId - 2].v[2] = verts[thrId];
+		}
+	}
+}
+
+__global__ void kernRasterize(Triangle* tris, Fragment* buf, int width, int height, int triCount) {
+	int thrId = (blockIdx.x * blockDim.x) + threadIdx.x;
+	if (thrId < triCount) {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				float tempX = ((x / (float)width) * 2.0f) - 1.0f;
+				float tempY = ((y / (float)height) * 2.0f) - 1.0f;
+				glm::vec3 triangle[3];
+				triangle[0] = tris[thrId].v[0].pos;
+				triangle[1] = tris[thrId].v[1].pos;
+				triangle[2] = tris[thrId].v[2].pos;
+				glm::vec3 baryCoord = calculateBarycentricCoordinate(triangle, glm::vec2(tempX, tempY));
+				if(isBarycentricCoordInBounds(baryCoord)) {
+					buf[x + y*width].color = glm::vec3(1.0f, 1.0f, 1.0f);
+				}
+				else {
+					buf[x + y*width].color = glm::vec3(0.0f, 0.0f, 0.0f);
+				}
+			}
+		}
+	}
 }
 
 /**
@@ -132,6 +185,18 @@ void rasterize(uchar4 *pbo) {
 
     // TODO: Execute your rasterization pipeline here
     // (See README for rasterization pipeline outline.)
+
+	//Set buffer to default value
+	cudaMemset(dev_depthbuffer, 0, width * height * sizeof(Fragment));
+	
+	//Transfer from VertexIn to VertexOut (vertex shading)
+	vertexShader<<<1, vertCount>>>(dev_bufVertex, dev_bufTransformedVertex, vertCount);
+
+	//Transfer from VertexOut to Triangles (primitive assembly)
+	primitiveAssemble<<<1, vertCount>>>(dev_bufTransformedVertex, dev_primitives, vertCount);
+
+	//Scanline each triangle to get fragment color (rasterize)
+	kernRasterize<<<1, vertCount / 3>>>(dev_primitives, dev_depthbuffer, width, height, vertCount/3);
 
     // Copy depthbuffer colors into framebuffer
     render<<<blockCount2d, blockSize2d>>>(width, height, dev_depthbuffer, dev_framebuffer);
