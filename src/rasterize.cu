6e2a86da101f946f24399ee9171397045f029a0c
Bradley Crusco <bcrusco627@gmail.com>
2015-10-05 16:54:51 -0400

    Some of the basic parts of the first run pipeline.

--- src/rasterize.cu
+++ src/rasterize.cu
@@ -23,19 +23,24 @@ struct VertexIn {
     // TODO (optional) add other vertex attributes (e.g. texture coordinates)
 };
 struct VertexOut {
-    // TODO
+    // TODO: Mirrors VertexIn?
+	glm::vec3 pos;
+	glm::vec3 nor;
+	glm::vec3 col;
 };
 struct Triangle {
     VertexOut v[3];
 };
 struct Fragment {
     glm::vec3 color;
+	// will probably need to add more here? Will require updating my clear method
 };
 
 static int width = 0;
 static int height = 0;
 static int *dev_bufIdx = NULL;
-static VertexIn *dev_bufVertex = NULL;
+static VertexIn *dev_bufVertex = NULL; //TODO Shouldn't this really be changed to indicate that it is in?
+static VertexOut *dev_bufVertexOut = NULL;
 static Triangle *dev_primitives = NULL;
 static Fragment *dev_depthbuffer = NULL;
 static glm::vec3 *dev_framebuffer = NULL;
@@ -77,6 +82,66 @@ void render(int w, int h, Fragment *depthbuffer, glm::vec3 *framebuffer) {
 }
 
 /**
+ * Clears the depth buffer with a black color.
+ */
+__global__
+void clearDepthBuffer(int w, int h, Fragment *depthbuffer) {
+	// TODO: Block stuff wrong?
+	int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+	int index = x + (y * w);
+
+	if (x < w && y < h) {
+		depthbuffer[index].color = glm::vec3(0.0f);
+	}
+}
+
+/**
+ * Apply vertex transformations and transfer to vertex out buffer
+ */
+__global__
+void vertexShading(int w, int h, VertexIn *vertexBufferIn, VertexOut *vertexBufferOut) {
+	// TODO: Block stuff wrong?
+	int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+	int index = x + (y * w);
+
+	// in this iteration just copy shit over. Later iterations will do transforms
+	if (x < w && y < h) {
+		vertexBufferOut[index].col = vertexBufferIn[index].col;
+		vertexBufferOut[index].nor = vertexBufferIn[index].nor;
+		vertexBufferOut[index].pos = vertexBufferIn[index].pos;
+	}
+}
+
+/**
+ * Assemble primitives from vertex out buffer data.
+ */
+__global__
+void assemblePrimitives(int w, int h, VertexOut *vertexBufferOut, Triangle *primitives) {
+	// TODO: Block stuff is WAY wrong here. 
+	// Currently only supports triangles
+	int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+	int index = x + (y * w);
+
+	if (x < w && y < h) {
+		Triangle primitive = primitives[index / 3];
+		for (int i = 0; i < 3; i++) {
+			primitive.v[i] = vertexBufferOut[3 * index + i]; // three times as many verts as triangles, gotta offset by index
+			// You're getting the 3 verts that make up a triangle
+		}
+	}
+}
+
+__global__
+void scanlineRaserization(int w, int h, Triangle *primitives, Fragment *depthbuffer) {
+	// this is going to be the most incomplete one.
+	// how large is hte depthbuffer vs the number of primitives? how do I insert them?
+	// which buffer are they going to right now?
+}
+
+/**
  * Called once at the beginning of the program to allocate memory.
  */
 void rasterizeInit(int w, int h) {
@@ -115,6 +180,10 @@ void rasterizeSetBuffers(
     cudaMalloc(&dev_bufVertex, vertCount * sizeof(VertexIn));
     cudaMemcpy(dev_bufVertex, bufVertex, vertCount * sizeof(VertexIn), cudaMemcpyHostToDevice);
 
+	cudaFree(dev_bufVertexOut);
+	cudaMalloc(&dev_bufVertexOut, vertCount * sizeof(VertexOut));
+	cudaMemset(dev_bufVertexOut, 0, vertCount * sizeof(VertexIn));
+
     cudaFree(dev_primitives);
     cudaMalloc(&dev_primitives, vertCount / 3 * sizeof(Triangle));
     cudaMemset(dev_primitives, 0, vertCount / 3 * sizeof(Triangle));
@@ -133,13 +202,24 @@ void rasterize(uchar4 *pbo) {
 
     // TODO: Execute your rasterization pipeline here
     // (See README for rasterization pipeline outline.)
-
-
+	
 
 	//so each stage of the pipeline is just anotehr kernel function?
+	// Or should they be combined somewhat into one or two kerns?
 
+	// first clear the depth buffer with some default value (0?)
+	// TODO: Block stuff wrong?
+	clearDepthBuffer<<<blockCount2d, blockSize2d>>>(width, height, dev_depthbuffer);
 
+	// Vertex shading
+	// TODO: Block stuff wrong?
+	vertexShading<<<blockCount2d, blockSize2d>>>(width, height, dev_bufVertex, dev_bufVertexOut);
 
+	// Primitive Assembly
+	// TODO: Block stuff wrong?
+	assemblePrimitives<<<blockCount2d, blockSize2d>>>(width, height, dev_bufVertexOut, dev_primitives);
+
+	// rasterization
 
     // Copy depthbuffer colors into framebuffer
     render<<<blockCount2d, blockSize2d>>>(width, height, dev_depthbuffer, dev_framebuffer);
