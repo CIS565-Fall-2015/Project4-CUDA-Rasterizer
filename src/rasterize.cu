c938e160c904c4f05819b03b2ae2523564c6c9f4
Sally Kong <kongsally@users.noreply.github.com>
2015-10-11 23:55:47 -0400

    tried adding thrust random, but no

--- src/rasterize.cu
+++ src/rasterize.cu
@@ -38,7 +38,25 @@ struct Fragment {
     glm::vec3 color;
 	glm::vec3 position;
 	glm::vec3 normal;
+	int depth;
 };
+
+__host__ __device__ inline unsigned int utilhash(unsigned int a) {
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+}
+
+
+__host__ __device__
+thrust::default_random_engine makeSeededRandomEngine(int iter, int index, int depth) {
+    int h = utilhash((1 << 31) | (depth << 22) | iter) ^ utilhash(index);
+    return thrust::default_random_engine(h);
+}
 
 static int width = 0;
 static int height = 0;
@@ -81,6 +99,17 @@ __global__ void render(int w, int h, Fragment *depthbuffer, glm::vec3 *framebuff
     if (x < w && y < h) {
         framebuffer[index] = depthbuffer[index].color;
     }
+}
+
+__global__ void depthBufferClearing(int w, int h, Fragment *fragments) {
+	int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+    int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+    int index = x + (y * w);
+
+	if ( x < w && y < h) {
+		fragments[index].depth = INT_MAX;
+		fragments[index].color = glm::vec3(0.0f);
+	}
 }
 
 __global__ void vertexShading(int n, glm::mat4 view_projection,
@@ -135,8 +164,8 @@ __global__ void rasterizing(int n, int w, int h,
 	int index = (blockIdx.x * blockDim.x) + threadIdx.x;
     if (index < n) {
 		Triangle tri = primitives[index];
-		glm::vec3 vertices[3] = {tri.v[0].pos, tri.v[1].pos, tri.v[2].pos};
-		AABB aabb = getAABBForTriangle(vertices);
+		glm::vec3 tri_verts[3] = {tri.v[0].pos, tri.v[1].pos, tri.v[2].pos};
+		AABB aabb = getAABBForTriangle(tri_verts);
 
 		glm::vec2 pixel_min;
 		pixel_min.x = (aabb.min.x + 1) * w / 2.0f;
@@ -149,24 +178,36 @@ __global__ void rasterizing(int n, int w, int h,
 		for (int i = glm::max(0.0f, pixel_min.x); i <= pixel_max.x; i++) {
 			for (int j = glm::max(0.0f, pixel_min.y); j <= pixel_max.y; j++) {
 				
-				float x = (i/float(w)) * 2.0f - 1;
-				float y = (j/float(h)) * 2.0f - 1;
+				thrust::default_random_engine rng = makeSeededRandomEngine(0, index, 0);
+				thrust::uniform_real_distribution<float> u01(0, 1);
 
-				glm::vec3 barycentric = calculateBarycentricCoordinate(vertices,
+				float x = ((i + u01(rng))/float(w)) * 2.0f - 1;
+				float y = ((j + u01(rng))/float(h)) * 2.0f - 1;
+
+				glm::vec3 barycentric = calculateBarycentricCoordinate(tri_verts,
 					glm::vec2(x,y));
 				if (isBarycentricCoordInBounds(barycentric)) {
-					Fragment frag;
-					frag.color = (primitives[index].v[0].col
-						+ primitives[index].v[1].col
-						+ primitives[index].v[2].col) / 3.0f;
 
-					frag.normal = (primitives[index].v[0].nor
-						+ primitives[index].v[1].nor
-						+ primitives[index].v[2].nor) / 3.0f;
+					int frag_index = j*w + i;
+					int depth = getZAtCoordinate(barycentric, tri_verts) * INT_MAX;
+					atomicMin(&fs_input[frag_index].depth, depth);
+					
+					if(fs_input[frag_index].depth == depth) {
 
-					frag.position = barycentric;
+						Fragment frag;
+						frag.color = (primitives[index].v[0].col
+							+ primitives[index].v[1].col
+							+ primitives[index].v[2].col) / 3.0f;
 
-					fs_input[j*w + i] = frag;
+						frag.normal = (primitives[index].v[0].nor
+							+ primitives[index].v[1].nor
+							+ primitives[index].v[2].nor) / 3.0f;
+
+						frag.position = barycentric;
+						frag.depth = depth;
+
+						fs_input[frag_index] = frag;
+					}
 				}
 			}
 		}
@@ -188,15 +229,6 @@ __global__ void fragmentShading(int w, int h, Fragment *fs, glm::vec3 light_pos)
 			glm::normalize(light_pos - fs[index].position)));
 	}
 
-}
-
-__global__ void fragmentsToDepth() {
-}
-
-__global__ void depthTesting() {
-}
-
-__global__ void fragmentToBuffer() {
 }
 
 /**
@@ -263,7 +295,13 @@ void rasterize(uchar4 *pbo) {
 	dim3 blockSize1d(128);
 	dim3 blockCount1d((vertCount + 128 - 1) / 128);
 
-    // TODO: Execute your rasterization pipeline here
+    //-----RATERIZATION PIPELINE----------
+
+	//---Clear Depth Buffer
+	depthBufferClearing<<<blockCount2d, blockSize2d>>>(width, height, dev_depthbuffer);
+	checkCUDAError("depth buffer clearing");
+
+	//---Vertex Shader
 	//view matrix 
 	glm::mat4 view = glm::lookAt(
 			glm::vec3(0.0f, 1.5f, 5.0f), 
@@ -280,14 +318,17 @@ void rasterize(uchar4 *pbo) {
 		dev_bufVertex, dev_bufVertex_out);
 	checkCUDAError("vertex shader");
 
+	//---Primitive Assembly
 	primitiveAssembling<<<blockCount1d, blockSize1d>>>(vertCount/3, 
 		dev_bufVertex_out, dev_primitives);
 	checkCUDAError("primitive assembling");
 
+	//---Rasterization
 	rasterizing<<<blockCount1d, blockSize1d>>>(vertCount/3, width, height,
 		dev_primitives, dev_depthbuffer);
 	checkCUDAError("triangle rasterizing");
 
+	//--Fragment Shader
 	glm::vec3 light_pos = glm::vec3(-3.0f, 5.0f, 10.0f);
 	fragmentShading<<<blockCount2d, blockSize2d>>>(width, height, dev_depthbuffer,
 		light_pos);
